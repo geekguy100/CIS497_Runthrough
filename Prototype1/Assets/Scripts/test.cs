f8f870a8d1cca46a850cb1f992915256478348dc
Kyle Grenier <kylegrenier@users.noreply.github.com>
2020-09-04 12:04:15 -0500

    Added test.cs for testing new headers

---
+++ Prototype1/Assets/Scripts/test.cs
@@ -0,0 +1,21 @@
+/*****************************************************************************
+// File Name :         test.cs
+// Author :            Kyle Grenier
+// Creation Date :     September 04, 2020
+//
+// Brief Description : ADD BRIEF DESCRIPTION OF THE FILE HERE
+*****************************************************************************/
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

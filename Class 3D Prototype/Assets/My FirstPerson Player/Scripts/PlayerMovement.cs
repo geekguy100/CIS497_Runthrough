fb3774743e7180466a4a6265e34f2766c4d514b3
Kyle Grenier <kylegrenier@users.noreply.github.com>
2020-10-16 19:28:52 -0500

    Player movement and falling

--- Class 3D Prototype/Assets/My FirstPerson Player/Scripts/PlayerMovement.cs
+++ Class 3D Prototype/Assets/My FirstPerson Player/Scripts/PlayerMovement.cs
@@ -12,15 +12,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private CharacterController controller;
+    [SerializeField] private float speed = 12f;
+
+    [SerializeField] private Vector3 velocity;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float gravityMultiplier = 2f;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+
+    private void Awake()
     {
-        
+        controller = GetComponent<CharacterController>();
+        gravity *= gravityMultiplier;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, whatIsGround);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        velocity.y += gravity * Time.deltaTime;
         
+        
+
+        controller.Move(velocity * Time.deltaTime);
+        controller.Move(move * speed * Time.deltaTime);
     }
 }

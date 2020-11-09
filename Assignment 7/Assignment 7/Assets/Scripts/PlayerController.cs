49e144da26e3ebbfc7e03ffa679176eedf284c78
Kyle Grenier <kylegrenier@users.noreply.github.com>
2020-11-08 19:42:17 -0600

    Player powerups.

--- Assignment 7/Assignment 7/Assets/Scripts/PlayerController.cs
+++ Assignment 7/Assignment 7/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@
 // Brief Description : Player controller script; controls player via button input.
 *****************************************************************************/
 using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
@@ -14,6 +15,10 @@ public class PlayerController : MonoBehaviour
 
     private float forwardInput;
     private GameObject focalPoint;
+
+    [SerializeField] private GameObject powerupIndicator;
+    private bool hasPowerup = false;
+    private float powerupStength = 15.0f;
 
     private void Awake()
     {
@@ -24,10 +29,49 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         forwardInput = Input.GetAxis("Vertical");
+
+        //Update powerup indicator's position to the player.
+        //The vector3 we add to the player's position is an offet.
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
     private void FixedUpdate()
     {
         rb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    private IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7f);
+        hasPowerup = false;
+        powerupIndicator.SetActive(true);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Player has collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+
+            //Get a local reference to the enemy rigidbody.
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+
+            //Set a vector3 in the direction AWAY from the player.
+            Vector3 awayFromPlayer = (collision.transform.position - transform.position).normalized;
+
+            //Add an impulse force to the enemy away from the player.
+            enemyRb.AddForce(awayFromPlayer * powerupStength, ForceMode.Impulse);
+        }
     }
 }
